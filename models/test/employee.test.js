12c6a2f4bbbde66ee0edcfd0b5030ab348292992
Karolina Jurczak <inka_c@tlen.pl>
2020-07-23 15:34:08 +0200

    Fix bug at database adress

--- models/test/employee.test.js
+++ models/test/employee.test.js
@@ -14,12 +14,12 @@ describe('Employee', () => {
 
   it('should throw an error if arg is not a string', () => {
     const cases = [
-      {firstName: 'John', lastName:'Doe', department: 'undefined'}, {firstName: 'John', lastName:'Doe', department: 0},
+      {firstName: 'John', lastName:'Doe', department: undefined}, 
       {firstName: 'John', lastName:'Doe', department: []},
       {firstName: 'John', lastName:'Doe', department: null},
     ];
     for(let arg of cases) {
-      const employee = new Employee({ arg });
+      const employee = new Employee( arg );
   
       employee.validate(err => {
         expect(err.errors.department).to.exist;
